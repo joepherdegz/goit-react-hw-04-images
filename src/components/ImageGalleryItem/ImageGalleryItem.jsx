e2a24acbaf7dcf6a39b6ce75fcb0bafc38584867
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-06-27 20:21:22 -0700

    update

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,38 +1,30 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import css from './ImageGalleryItem.module.css';
-import ImageModal from 'components/Modal/Modal'
+import ImageModal from 'components/Modal/Modal';
 
-export class ImageGalleryItem extends Component {
-    state = {
-        selectedImage: null,
-    };
+export const ImageGalleryItem = ({ webformatURL, largeImageURL, tags }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
 
-    handleOpenModal = () => {
-        this.setState({
-            selectedImage: this.props.largeImageURL,
-        });
-    };
+  const handleOpenModal = () => {
+    setSelectedImage(largeImageURL);
+  };
 
-    handleCloseModal = () => {
-        this.setState({ selectedImage: null})
-    }
-    render() {
-        const { selectedImage } = this.state;
-    const { webformatURL, tags } = this.props;
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
 
-    return (
-      <li className={css.imageGalleryItem} onClick={this.handleOpenModal}>
-        <img
-          className={css.imageGalleryItemImage}
-          src={webformatURL}
-          alt={tags}
-            />
-            <ImageModal
-                modalClose={this.handleCloseModal}
-                modalOpen={selectedImage !== null}
-                image={selectedImage}
-            />
-      </li>
-    );
-  }
-}
+  return (
+    <li className={css.imageGalleryItem} onClick={handleOpenModal}>
+      <img
+        className={css.imageGalleryItemImage}
+        src={webformatURL}
+        alt={tags}
+      />
+      <ImageModal
+        modalClose={handleCloseModal}
+        modalOpen={selectedImage !== null}
+        image={selectedImage}
+      />
+    </li>
+  );
+};
